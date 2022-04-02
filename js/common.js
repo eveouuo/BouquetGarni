93f317885fca8fa2f0e7ea3f976bf8d6f012daa6
eve <lovelylove.eve@gmail.com>
2022-04-02 16:28:26 +0900

    220402 commit!

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    $("header nav ul.gnb > li").hover(function(){
+        $(this).find(".sub,.subArea").stop().slideDown();
+        $(".gnbBG").stop().slideDown();
+    },function(){
+        $(this).find(".sub,.subArea").stop().slideUp();
+        $(".gnbBG").stop().slideUp();
+    });
 });
